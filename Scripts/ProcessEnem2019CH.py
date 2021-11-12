9707877d3c2281670fb6912835e4f9699d31a3cc
Vitoria Guardieiro <vitoriaguardieiro@gmail.com>
2021-11-12 19:28:41 -0300

    Fix error in ProcessEnem2019CH.py

--- Scripts/ProcessEnem2019CH.py
+++ Scripts/ProcessEnem2019CH.py
@@ -1,6 +1,6 @@
 from ProcessEnemDataClass import ProcessEnemData
 
-egb = ProcessEnemData(ano=2019, competencia='CH', questoes_anuladas=['45'], caderno='Azul', feat_grupo='TP_COR_RACA')
-enem_df = egb.get_data()
+penem = ProcessEnemData(ano=2019, competencia='CH', questoes_anuladas=['45'], caderno='Azul', feat_grupo='TP_COR_RACA')
+enem_df = penem.get_data()
 
 grupo_df = penem.filter_data()
