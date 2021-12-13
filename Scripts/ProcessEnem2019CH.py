4292315eed71c8990c2ff858397c62f9806b9672
Vitoria Guardieiro <vitoriaguardieiro@gmail.com>
2021-12-13 17:49:16 -0300

    Edit ProcessEnem2019CH

--- Scripts/ProcessEnem2019CH.py
+++ Scripts/ProcessEnem2019CH.py
@@ -1,6 +1,6 @@
 from ProcessEnemDataClass import ProcessEnemData
 
-penem = ProcessEnemData(ano=2019, competencia='CH', questoes_anuladas=['45'], caderno='Azul', feat_grupo='TP_COR_RACA')
+penem = ProcessEnemData(ano=2019, competencia='CH', questoes_anuladas=['45'], feat_grupo='TP_COR_RACA')
 enem_df = penem.get_data()
 
 grupo_df = penem.filter_data()
