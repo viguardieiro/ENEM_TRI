5ec27034c4ee067aaed3b666a857f2c60ec23d79
Vitoria Guardieiro <vitoriaguardieiro@gmail.com>
2022-01-11 18:15:38 -0300

    Add region analysis for 2017

---
+++ Scripts/ProcessEnem2017.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from ProcessEnemDataClass import ProcessEnemData
+
+penem = ProcessEnemData(ano=2017)
+
+# Processa dados de todos
+print("[INFO] Processando dados de todos os alunos...")
+enem_df = penem.get_data()
+print("[INFO] Processamento dos dados de todos os alunos concluído.")
+
+competencias = ['CN', 'CH', 'LC', 'MT']
+itens_anulados = {'CN': [],
+                  'CH': [],
+                  'LC': [],
+                  'MT': []}
+
+for comp in competencias:
+    penem.process_competence(comp, itens_anulados[comp], all_p=True, concluintes=True)
+    
+df_grupo = penem.get_group_features()
+
+penem.process_region_competence()
